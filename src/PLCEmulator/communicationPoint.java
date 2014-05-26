7ae62ed19f94b42319ada838d580e64f28b060c8
Andre <freak4365@gmail.com>
2014-05-26 16:31:16 +0200

    I forgot to save.

--- src/PLCEmulator/communicationPoint.java
+++ src/PLCEmulator/communicationPoint.java
@@ -1,5 +1,14 @@
 package PLCEmulator;
 
+/**
+ * @author André Kukla
+ *
+ */
 public class communicationPoint {
-
+	
+//	private String name;
+//	private int pos_x;
+//	private int pos_y;
+//	private String next;
+//	private String prev;
 }
